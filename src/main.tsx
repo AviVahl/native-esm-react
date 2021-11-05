3733a24520062b60b5ea7f706abe470e5454ba9c
Avi Vahl <avi.vahl@wix.com>
2021-11-05 23:10:00 +0200

    feat: use StrictMode for rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { HelloWorld } from "./hello-world.js";
 
 const container = document.body.appendChild(document.createElement("div"));
-ReactDOM.render(<HelloWorld />, container);
+ReactDOM.render(
+  <StrictMode>
+    <HelloWorld />
+  </StrictMode>,
+  container
+);
