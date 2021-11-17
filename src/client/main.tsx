4ff3797fb9906b16dd14256ed1ba5ba5cf0df97e
Avi Vahl <avi.vahl@wix.com>
2021-11-17 21:01:17 +0200

    feat: prepare for SSR

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,11 +1,26 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app.js";
+import { App } from "./app.jsx";
 
-const container = document.body.appendChild(document.createElement("div"));
-ReactDOM.render(
+const rootContainerId = "SITE_MAIN";
+
+const container =
+  document.getElementById(rootContainerId) ?? createContainer(document.body);
+
+const appReactElement = (
   <StrictMode>
     <App />
-  </StrictMode>,
-  container
+  </StrictMode>
 );
+
+if (container.hasAttribute("data-ssr")) {
+  ReactDOM.hydrate(appReactElement, container);
+} else {
+  ReactDOM.render(appReactElement, container);
+}
+
+function createContainer(targetParent: Element) {
+  const newContainer = document.createElement("div");
+  newContainer.id = rootContainerId;
+  return targetParent.appendChild(newContainer);
+}
