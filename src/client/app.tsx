6cf4cbe825cafac7ce43c9b4e6c8fff055cd00e6
Avi Vahl <avi.vahl@wix.com>
2022-01-29 14:03:49 +0200

    feat: add alt to imgs

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -6,8 +6,8 @@ const html5Logo = new URL("../../static/html5-logo.svg", import.meta.url);
 export function App() {
   return (
     <div style={{ textAlign: "center", paddingTop: "5em" }}>
-      <img width={120} height={120} src={reactLogo.href}></img>
-      <img width={120} height={120} src={html5Logo.href}></img>
+      <img width={120} height={120} src={reactLogo.href} alt="react"></img>
+      <img width={120} height={120} src={html5Logo.href} alt="html5"></img>
     </div>
   );
 }
